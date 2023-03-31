d8610c5ab87ab3f0bc9553c0c6e1f0e132f5fb1d
Borna <bpinteric@pmfst.hr>
2023-03-31 14:45:46 +0200

    Vj 4

---
+++ Vjezbe/Vjezbe_4/gibanje.py
@@ -0,0 +1,11 @@
+import particle as prt
+import numpy as np
+p1=prt.Particle(50.,45,3,2)
+#y0+v0t+1/2gt2=0
+v0=p1.v*np.cos(p1.k/180.*np.pi)
+t=(-v0-np.sqrt(v0**2-2*p1.ay*p1.y))/p1.ay
+d=v0*t
+x=abs(d-p1.range())
+print("numericki domet: {}".format(p1.range()))
+print("analiticki domet: {}".format(d))
+print("pogreska: {}".format(x))
---
+++ Vjezbe/Vjezbe_4/grafrel.py
@@ -0,0 +1,12 @@
+import particle as prt
+import numpy as np
+import matplotlib.pyplot as plt
+p1=prt.Particle(10,60,0,0)
+gr=[]
+v0=p1.v*np.cos(p1.k/180.*np.pi)
+t=(-v0-np.sqrt(v0**2-2*p1.ay*p1.y))/p1.ay
+d=v0*t
+for dt in np.linspace(0.01,0.1,100):
+    gr.append(abs(d-p1.range(dt))/d*100)
+plt.plot(np.linspace(0.01,0.1,100),gr)
+plt.show()
