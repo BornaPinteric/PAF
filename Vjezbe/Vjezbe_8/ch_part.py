1cc73063fd7f1497a862d6aca45d5aa26db59a71
Borna <bpinteric@pmfst.hr>
2023-05-12 12:28:26 +0200

    Ostatak vj8

--- Vjezbe/Vjezbe_8/ch_part.py
+++ Vjezbe/Vjezbe_8/ch_part.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 class ChargedParticle:
     def __init__(self,v0,q,m):
         self.v=np.array(v0)
@@ -16,17 +16,30 @@ class ChargedParticle:
         self.v=self.v+a*dt
         self.pos=self.pos+self.v*dt
     def plot_tr(self,E,B,dt=0.01):
+        fig=plt.figure()
+        ax=fig.add_subplot(1,1,1,projection="3d")
         T=0
-        n=0
         POS=[]
         POS.append((0,0,0))
-        while n<4 and T<50:
+        while T<50:
             self.__move(E,B,dt)
             POS.append(self.pos)
             T+=dt
-            if((self.pos<self.v*dt).all()):
-                n+=1
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.plot3D([p[0] for p in POS],[p[1] for p in POS],[p[2] for p in POS])
+        self.reset()
+        ax.plot3D([p[0] for p in POS],[p[1] for p in POS],[p[2] for p in POS],label="Charged particle")
+        self.q*=(-1)
+        T=0
+        POS=[]
+        POS.append((0,0,0))
+        v0=self.v
+        while T<50:
+            self.__move(E,B,dt)
+            POS.append(self.pos)
+            T+=dt
+        self.reset()
+        ax.plot3D([p[0] for p in POS],[p[1] for p in POS],[p[2] for p in POS],label="Charged particle (opposite charge)")
+        ax.set_xlabel("x[m]")
+        ax.set_ylabel("y[m]")
+        ax.set_zlabel("z[m]")
+        plt.legend()
         plt.show()
--- Vjezbe/Vjezbe_8/test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-v=np.array((1,1,1))
-x=(0,0,0)
-a=x+v*0.01
-print([v,a,a+v*0.01])
